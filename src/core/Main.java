4c3f24afc7d6b072ecb02cee0d89cec78680af48
Monika <medanielewicz@wpi.edu>
2016-03-17 23:44:45 -0400

    fixed the math

--- src/core/Main.java
+++ src/core/Main.java
@@ -3,6 +3,6 @@ package core;
 public class Main {
 	public static void main(String[]args){
 		System.out.println("Hello World!");
-		System.out.println("2+2=5");
+		System.out.println("2+2=4");
 	}
 }
